5a7bf849d75254be702ab82810a4184911f11aaa
Geoff <geoffkellync@gmail.com>
2022-04-19 10:24:04 -0400

    added styling changes

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -50,10 +50,30 @@ const StyledIcons = styled.div`
     .Email-icon::after {
         content: "";
         display: block;
-        width: 1px;
+        width: 2px;
         height: 6em;
         margin: 0px auto;
-        background-color: ${pr => pr.theme.colors.primary};
+        background: linear-gradient(
+        60deg,
+        hsl(224, 85%, 66%),
+        hsl(269, 85%, 66%),
+        hsl(314, 85%, 66%),
+        hsl(359, 85%, 66%),
+        hsl(44, 85%, 66%),
+        hsl(89, 85%, 66%),
+        hsl(134, 85%, 66%),
+        hsl(179, 85%, 66%)
+        );
+        background-size: 300% 300%;
+        background-position: 0 50%;
+        border-radius: calc(2 * var(--border-width));
+        animation: moveGradient 4s alternate infinite;
+    }
+
+    @keyframes moveGradient {
+        50% {
+            background-position: 100% 50%;
+        }
     }
 
     .Email-icon::after:hover {
