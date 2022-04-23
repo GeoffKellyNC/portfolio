c98a2fdc2f240e4c4b4992e5a43e9d49bbd1bc12
Geoff <geoffkellync@gmail.com>
2022-04-23 17:42:09 -0400

    updated .env variables

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 
    
-export const EMAIL_JS_KEY = 'a3WaIZCbS3IngdwBh'
+export const EMAIL_JS_KEY = process.env.REACT_APP_EMAIL_JS_KEY;
 
-export const TEMPLATE_ID = 'template_up1mgzr'
+export const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
 
-export const SERVICE_ID = 'service_nmhnnvi'
+export const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
